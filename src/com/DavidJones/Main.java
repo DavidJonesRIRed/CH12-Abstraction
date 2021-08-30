f96c1da223e8a316d0b2ebb16484fa1b75fead62
David Jones <coreldavid@hotmail.com>
2021-08-30 19:06:31 -0400

    Vehicle class works

--- src/com/DavidJones/Main.java
+++ src/com/DavidJones/Main.java
@@ -1,8 +1,12 @@
 package com.DavidJones;
 
+import com.Vehicle.Vehicle;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Vehicle myVehicle = new Vehicle();
+        myVehicle.startEngine();
     }
 }
